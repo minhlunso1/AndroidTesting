45cd6497129b10485e08856531342bd9cf102977
Minh <aminh1603@gmail.com>
2017-08-12 16:02:38 +0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android Testing
+
+Author: **Minh Nguyen**
+
+## I. Unit Test
+1/ 
+    A queue contains a number of people who want to buy tickets. Each buying time takes 1 seconds
+    and in each turn can buy only one ticket. To continue, that person must be enqueued in the last list.
+    The best algorithm BigO(n).
---
+++ app/src/test/java/minhna/androidtesting/TimeTicketQueueTest.java
@@ -0,0 +1,69 @@
+package minhna.androidtesting;
+
+/**
+ * Created by Administrator on 12-Aug-17.
+ */
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+    A queue contains a number of people who want to buy tickets. Each buying time takes 1 seconds
+    and in each turn can buy only one ticket. To continue, that person must be enqueued in the last list.
+    The best algorithm BigO(n)
+ */
+public class TimeTicketQueueTest {
+    int position;
+    int[] listTicketQueue;
+
+    @Before
+    public void setUp() {
+        position = 2;
+        listTicketQueue = getTicketQueue();
+    }
+
+    @Test
+    public void timeBuyingTicket_isCorret() throws Exception {
+        int queueSize = listTicketQueue.length;
+
+        int beforeCount = getListCountBefore(position);
+        int afterCount = getListCountAfter(position, queueSize);
+        int numberOfTicketInTargetPos = listTicketQueue[2];
+
+        int time = beforeCount + afterCount + numberOfTicketInTargetPos;
+        System.out.println("Before (s): " + beforeCount);
+        System.out.println("After (s): " + afterCount);
+        System.out.println("Time (s): " + time);
+        assertEquals(11, time);
+    }
+
+    private int getListCountAfter(int position, int queueSize) {
+        int afterCountList = 0;
+        for (int i = position + 1; i < queueSize; i++) {
+            if (listTicketQueue[position] - listTicketQueue[i] >= 0)
+                afterCountList += listTicketQueue[i];
+            else
+                afterCountList += listTicketQueue[position];
+        }
+        return afterCountList;
+    }
+
+    private int getListCountBefore(int position) {
+        int beforeCountList = 0;
+        for (int i = position - 1; i >= 0; i--) {
+            if (listTicketQueue[position] - listTicketQueue[i] >= 0)
+                beforeCountList += listTicketQueue[i];
+            else
+                beforeCountList += listTicketQueue[position] - 1;
+        }
+        return beforeCountList;
+    }
+
+    public int[] getTicketQueue() {
+        //3 2 3 1 -> 5
+        //2 1 2 -> 9
+        //[0] 2 1 -> 11
+        return new int[]{1, 4, 3, 4, 2};
+    }
+}
