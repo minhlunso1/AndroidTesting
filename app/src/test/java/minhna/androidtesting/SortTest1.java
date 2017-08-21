006d4c517d7765fea03f58e859cafa532867154f
Minh <aminh1603@gmail.com>
2017-08-21 23:55:25 +0700

    unit_test_3

---
+++ app/src/test/java/minhna/androidtesting/SortTest1.java
@@ -0,0 +1,54 @@
+package minhna.androidtesting;
+
+/**
+ * Created by Administrator on 21-Aug-17.
+ */
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Sort list following the format: 1, n-1, 2, n-2 on descending list
+ * The best algorithm BigO(n)
+ */
+public class SortTest1 {
+    int[] list = new int[]{8, 7, 6, 5, 4, 3, 1};
+    int[] assertList = new int[]{1, 8, 3, 7, 4, 6, 5};
+    int[] list1 = new int[]{12, 10, 1, -3};
+    int[] assertList1 = new int[]{-3, 12, 1, 10};
+
+    public int[] sortList(int[] list) {
+        int[] result = new int[list.length];
+        int pivot = list.length / 2;
+        if (list.length % 2 != 0)
+            pivot++;
+        pivot--;
+        for (int i = list.length - 1, j = 0, k = 0; j < list.length ; i--, j++) {
+            if (i > pivot) {
+                result[j++] = list[i];
+                result[j] = list[k++];
+            } else if (pivot % 2 != 0 && i == pivot)
+                result[j] = list[i];
+        }
+        return result;
+    }
+
+    public boolean assertList(int[] list, int[] assertList) {
+        boolean isEquals = true;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != assertList[i]) {
+                isEquals = false;
+                break;
+            }
+        }
+        return isEquals;
+    }
+
+    @Test
+    public void testList() {
+        int[] sortedList = sortList(list);
+        Assert.assertEquals(assertList(sortedList, assertList), true);
+        Assert.assertEquals(assertList(sortList(list1), assertList1), true);
+    }
+}
